fbdf8a5375c28d2b2b19731830b9b5870d6b71c4
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-16 15:11:02 -0300

    arrumando detalhes no css

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,15 +6,15 @@ const Card = ({ title, description, tags, card_id, refresh }) => {
   const [modal, setModal] = useState()
 
   return (
-    <div className="mx-14 mb-5 border-2 p-10">
-      <div className="flex justify-between">
-        <h1>{title}</h1>
+    <div className="mx-14 w-[60vw] mb-5 border-2 p-10">
+      <div className="block md:flex md:justify-between">
+        <h1 className="text-xl pb-3 font-extrabold">{title}</h1>
         <button
           onClick={() => {
             setShow(true)
             setModal(modal)
           }}
-          className="m-3"
+          className="border-2 border-red-600 text-black px-4 py-2 rounded-md text-1xl font-medium hover:bg-red-600 transition duration-300"
         >
           Remove
         </button>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,39 +6,43 @@ const Header = ({ searchQuery, handleSearch, refresh }) => {
   const [modal, setModal] = useState()
 
   return (
-    <div className="">
-      <div className="pb-6">
-        <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">
+    <div>
+      <div className="p-4">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           VUTTR
         </h1>
-        <h3 className='"font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600"'>
+        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
           Very Useful Tools to Remember
-        </h3>
-        <span className="font-medium leading-tight text-base mt-0 mb-2 text-blue-600">
-          Ferramentas muito úteis para lembrar
-        </span>
+          <span className="text-sm">
+            {' ('}
+            Ferramentas muito úteis para lembrar{') '}
+          </span>
+        </p>
       </div>
-      <div className="flex justify-between m-4">
+      <div className="block mx-3 md:flex md:justify-between">
         <input
           type="search"
-          id="search"
-          className="border-2 p-1 border-slate-300 rounded-lg"
+          className={`w-full px-3 py-1.5 text-gray-700
+           bg-white border-2 border-solid border-gray-300 rounded 
+           transition ease-in-out focus:text-gray-700 mb-2
+           focus:bg-white md:w-3/6 md:h-12`}
           placeholder="Search"
-          required
           value={searchQuery}
           onChange={handleSearch}
         />
-        <div>
-          <button
-            onClick={() => {
-              setShow(true)
-              setModal(modal)
-            }}
-            className="m-3"
-          >
-            Add
-          </button>
-        </div>
+        <button
+          className={`px-6 py-2
+       border-2 border-green-600 text-green-600 
+       font-medium text-xs uppercase
+       rounded transition w-full mb-6
+       duration-150 ease-in-out md:w-2/6 md:h-12 hover:bg-green-600 hover:text-green-100`}
+          onClick={() => {
+            setShow(true)
+            setModal(modal)
+          }}
+        >
+          Adicionar ferramenta
+        </button>
       </div>
 
       <ModalAdd refresh={refresh} show={show} onClose={() => setShow(false)} />
