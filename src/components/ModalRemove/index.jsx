5cd6a3faa0b6bb3c0cf42adb45d6f988e542efae
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-16 19:45:54 -0300

    arrumando modal remove

--- src/components/ModalRemove/index.jsx
+++ src/components/ModalRemove/index.jsx
@@ -23,7 +23,7 @@ const ModalRemove = ({ show, onClose, id, name, setTools }) => {
             <div className="mt-3 sm:flex">
               <div className="p-6 text-center">
                 <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                  Are you sure you want to delete {name}?
+                  Tem certeza que quer deletar {name}?
                 </h3>
                 <button
                   data-modal-toggle="popup-modal"
@@ -31,7 +31,7 @@ const ModalRemove = ({ show, onClose, id, name, setTools }) => {
                   onClick={removeItem}
                   className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
                 >
-                  Yes, I'm sure
+                  Sim, eu tenho certeza.
                 </button>
                 <button
                   onClick={onClose}
@@ -39,7 +39,7 @@ const ModalRemove = ({ show, onClose, id, name, setTools }) => {
                   type="button"
                   className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                 >
-                  No, cancel
+                  Não, cancelar
                 </button>
               </div>
             </div>
