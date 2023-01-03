7dcb128e8521b8dbf098350fc54d723eccc57ad5
IsisG13 <isissouzaguimaraes@gmail.com>
2023-01-03 18:33:07 -0300

    adicionando login

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,17 @@ function App() {
   useEffect(() => {
     init()
   }, [])
-  if (!tools) return null
+  if (!tools)
+    return (
+      <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-600 opacity-75 flex flex-col items-center justify-center">
+        <h2 className="text-center text-white text-xl font-semibold">
+          Carregando...
+        </h2>
+        <p className="w-1/3 text-center text-white">
+          Talvez isso demore um pouco, não feche a página
+        </p>
+      </div>
+    )
 
   // Filtro de Pesquisa
   const handleSearch = (event) => {
