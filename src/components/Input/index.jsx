070801f802199463c895c3f2402acab7ff6fc623
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-02-01 16:34:03 -0300

    style: renomeando um atributo do input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Input = ({ label, nome, setFunc }) => {
+const Input = ({ label, value, setFunc }) => {
   return (
     <div>
       <label>{label}</label>
       <input
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-        value={nome}
-        onChange={(e) => setFunc(e.target.value)}
         required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+        value={value}
+        onChange={(e) => setFunc(e.target.value)}
       />
     </div>
   )
